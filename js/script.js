ddac4b67e3dbc737bade03496074da9a6c08ed41
silviuchiriac <6172099+silviuchiriac@users.noreply.github.com>
2020-06-04 17:17:25 +0200

    .

--- js/script.js
+++ js/script.js
@@ -10,6 +10,15 @@ var anim = bodymovin.loadAnimation({
   // path: 'https://assets2.lottiefiles.com/packages/lf20_94B4Hs.json'
 });
 
+bodymovin.loadAnimation({
+  container: lottieContainer2,
+  animType: 'svg',
+  autoplay: true,
+  loop: true,
+  path: 'assets/enlightened.json',
+});
+// https://lottiefiles.com/web-player?lottie_url=https%3A%2F%2Fassets6.lottiefiles.com%2Fpackages%2Flf20_EvfyyO.json
+
 function yeeah() {
   anim.goToAndPlay(0);
 };
