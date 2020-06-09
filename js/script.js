d8a9b18501805570e23257d36335e52168834dea
silviuchiriac <6172099+silviuchiriac@users.noreply.github.com>
2020-06-10 00:46:20 +0200

    .

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
+function toggleDarkMode() {
+  document.documentElement.classList.toggle('dark-mode');
+};
+
+var clock = new Date().getHours();
+if (clock >= 7 && clock <= 19) {
+  document.documentElement.classList.remove('dark-mode');
+} else {
+  document.documentElement.classList.add('dark-mode');
+}
+
 bodymovin.loadAnimation({
   container: illu,
   animType: 'svg',
   autoplay: true,
   loop: false,
-  path: 'assets/explosion.json',
+  path: 'https://assets9.lottiefiles.com/packages/lf20_G5TBf4.json',
 });
+
+// https://lottiefiles.com/18364-celebaration
 // https://lottiefiles.com/web-player?lottie_url=https%3A%2F%2Fassets6.lottiefiles.com%2Fpackages%2Flf20_EvfyyO.json
 // https://lottiefiles.com/23844-concept-man-flying-with-books
 
@@ -13,7 +26,11 @@ bodymovin.loadAnimation({
 // https://assets6.lottiefiles.com/private_files/lf30_jlRfsc.json
 // https://lottiefiles.com/4419-presto-loading-buddy-poppin
 // https://lottiefiles.com/111-xuanwheel-logo
-
+// https://assets9.lottiefiles.com/packages/lf20_BWSJId.json
+// https://lottiefiles.com/23402-question-boy
+// https://assets9.lottiefiles.com/packages/lf20_G5TBf4.json
+// https://lottiefiles.com/17594-brodie-jordan
+// https://lottiefiles.com/17415-push-to-talk
 
 var mouseContainer = document.body;
 var lottieContainer = document.getElementById('lottieContainer');
@@ -29,8 +46,4 @@ var anim = bodymovin.loadAnimation({
 
 function yeeah() {
   anim.goToAndPlay(0);
-};
-
-function toggleDarkMode() {
-  document.documentElement.classList.toggle('dark-mode');
 };
