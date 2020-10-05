5d8d932b42a14ad28174015f0b3c0aa2d2efd5f1
silviuchiriac <6172099+silviuchiriac@users.noreply.github.com>
2020-10-06 01:20:31 +0200

    .

--- js/index.js
+++ js/index.js
@@ -23,7 +23,7 @@ setTimeout(function(){
         // position: new Point(0,  view.size.height * 2),
         position: view.center,
         strokeColor: '#000',
-        strokeWidth: size * 0.005,
+        strokeWidth: size * 0.002,
         parent: circleGroup,
       });
     };
