20f3e813a61d3af6b7e8e9ea869f66cca72eef1d
silviuchiriac <6172099+silviuchiriac@users.noreply.github.com>
2020-10-08 09:36:22 +0200

    circles always red

--- js/index.js
+++ js/index.js
@@ -22,7 +22,7 @@ setTimeout(function(){
         radius: (size / interval) * (i * 3),
         // position: new Point(0,  view.size.height * 2),
         position: view.center,
-        strokeColor: '#000',
+        strokeColor: '#f40000',
         strokeWidth: size * 0.002,
         parent: circleGroup,
       });
