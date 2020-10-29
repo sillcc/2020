ed6ba66de1742fd325556caa2a1f12716b2d7337
silviuchiriac <6172099+silviuchiriac@users.noreply.github.com>
2020-10-29 16:10:44 +0100

    color

--- js/index.js
+++ js/index.js
@@ -22,7 +22,7 @@ setTimeout(function(){
         radius: (size / interval) * (i * 3),
         // position: new Point(0,  view.size.height * 2),
         position: view.center,
-        strokeColor: '#8819f7',
+        strokeColor: '#E21',
         strokeWidth: size * 0.002,
         parent: circleGroup,
       });
