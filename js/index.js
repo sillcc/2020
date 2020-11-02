523e793853ba349c6a4ad0edc2f4458d23b96a34
silviuchiriac <6172099+silviuchiriac@users.noreply.github.com>
2020-11-02 13:57:13 +0100

    stokeWidth fix

--- js/index.js
+++ js/index.js
@@ -23,7 +23,7 @@ setTimeout(function(){
         // position: new Point(0,  view.size.height * 2),
         position: view.center,
         strokeColor: '#E21',
-        strokeWidth: 1,
+        strokeWidth: size * 0.002,
         parent: circleGroup,
       });
     };
